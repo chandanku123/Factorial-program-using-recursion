867c89a724132b9277518cc2fc4ca27e52c3e92f
Chandanku123 <chandanpandeyprog@gmail.com>
2022-09-08 22:23:00 +0530

    Factorial code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+         int n = 4; //4,3,2,1
+        int result = Factorial(n);
+         Console.WriteLine(result);
+    }
+    
+ static int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int partialResult = Factorial(n-1);
+    return n * partialResult;
+}
+}
